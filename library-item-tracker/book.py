4f404f4936da738439059d5a2320c57869409cff
shippo24 <jade.valdor.1@ulaval.ca>
2026-05-10 12:17:13 -0400

    Add change_library_name and is_valid_title methods

--- library-item-tracker/book.py
+++ library-item-tracker/book.py
@@ -5,3 +5,11 @@ class Book:
         self.title = title
         self.author = author
         self.available = available
+
+@classmethod
+def change_library_name(cls, new_name):
+    cls.library_name = new_name
+
+@staticmethod
+def is_valid_title(title):
+    return len(title) > 0
