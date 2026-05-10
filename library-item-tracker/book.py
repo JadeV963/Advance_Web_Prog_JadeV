a83f1a9167f84c7cab7864ce1eaa1a199592d71f
shippo24 <jade.valdor.1@ulaval.ca>
2026-05-10 12:12:04 -0400

    Add Library_name class attribute

--- library-item-tracker/book.py
+++ library-item-tracker/book.py
@@ -1,5 +1,7 @@
 class Book:
+    library_name = "library of Paris"
     def __init__(self, title, author, available):
+        
         self.title = title
         self.author = author
         self.available = available
