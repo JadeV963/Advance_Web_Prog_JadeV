1565af33b5a0e697519a36287864dfec265cda51
shippo24 <jade.valdor.1@ulaval.ca>
2026-05-10 11:25:39 -0400

    Added one more book

--- library-item-tracker/main.py
+++ library-item-tracker/main.py
@@ -2,6 +2,8 @@ from book import Book
 
 book1 = Book("Le petit Prince", " Antoine De Saint_Exupéry", True)
 book2 = Book("Corto Maltese", "Hugo Pratt", False)
+book3 = Book("Dokkodo", "Miyamoto Musashi", True)
 
 print(book1.title, book1.author, book1.available)
 print(book2.title, book2.author, book2.available)
+print(book3.title, book3.author, book3.available)
