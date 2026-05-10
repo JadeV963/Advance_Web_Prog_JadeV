5e14fba6c045461f20da13ab5ff4e88aa5da4e5b
shippo24 <jade.valdor.1@ulaval.ca>
2026-05-10 11:38:49 -0400

    Add display_info instance method

--- library-item-tracker/book.py
+++ library-item-tracker/book.py
@@ -3,3 +3,17 @@ class Book:
         self.title = title
         self.author = author
         self.available = available
+
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print(f"{self.title} has been Borrowed!")
+        else:
+            print(f"{self.title} is not available.")
+    
+    def returned_book(self):
+        self.available = True
+        print(f"{self.title} has been returned!")
+
+    def display_info(self):
+        print(f"Title: {self.title}, Author:{self.author}, Available:{self.available}")
