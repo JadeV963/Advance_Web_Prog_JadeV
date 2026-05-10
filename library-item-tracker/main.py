1ed2ee0c134c96dc845687804c17537e618b0946
shippo24 <jade.valdor.1@ulaval.ca>
2026-05-10 11:19:20 -0400

    Add base Book class with instance attributes

---
+++ library-item-tracker/main.py
@@ -0,0 +1,7 @@
+from book import Book
+
+book1 = Book("Le petit Prince", " Antoine De Saint_Exupéry", True)
+book2 = Book("Corto Maltese", "Hugo Pratt", False)
+
+print(book1.title, book1.author, book1.available)
+print(book2.title, book2.author, book2.available)
